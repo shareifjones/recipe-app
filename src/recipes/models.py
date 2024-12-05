ff399dc6c2968bae5c4d723d263a59195d261dd1
shareifjones <shareifjones@gmail.com>
2024-12-04 22:53:07 -0500

    working on 2.5

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -7,6 +7,7 @@ class Recipe(models.Model):
     ingredients = models.CharField(max_length=255)
     cooking_time = models.IntegerField()
     difficulty = models.CharField(max_length=20)
+    pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
 
     def __str__(self):
         return f"Recipe ID: {self.id} - {self.name}"
