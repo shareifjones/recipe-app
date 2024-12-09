b589354c7610f4e31345dccebd67fbec36f3b944
shareifjones <shareifjones@gmail.com>
2024-12-08 22:59:10 -0500

    completed 2.6.

--- src/recipe_project/urls.py
+++ src/recipe_project/urls.py
@@ -19,10 +19,13 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import login_view, success_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls'))
+    path('', include('recipes.urls')),
+    path('login/', login_view, name='login'),
+    path('logout/', success_view, name='logout')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
