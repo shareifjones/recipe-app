4f764e8df623f07d6bb350d060959d292ea8675e
shareifjones <shareifjones@gmail.com>
2024-12-14 14:28:56 -0500

    troubleshooting the tests

--- src/recipes/tests.py
+++ src/recipes/tests.py
@@ -10,13 +10,7 @@ class RecipeModelTest(TestCase):
             name='Pasta', 
             ingredients='sauce, noodles',
             cooking_time=15,
-            difficulty='Easy'
-        )
-        self.recipe2 = Recipe.objects.create(
-            name='Pizza',
-            ingredients='cheese, dough',
-            cooking_time=20,
-            difficulty="Intermediate"
+            difficulty='Intermediate'
         )
 
     def test_recipe_creation(self):
@@ -54,47 +48,22 @@ class RecipeFormTest(TestCase):
         })
         self.assertTrue(form.is_valid())
 
-    def test_invalid_form(self):
-        form = RecipesSearchForm(data={
-            'recipe_name': '',
-            'ingredient_name': '',
-            'chart_type': ''
-        })
-        self.assertFalse(form.is_valid())
 
     def test_field_constraints(self):
         form = RecipesSearchForm(data={
-            'recipe_name': 'A' * 121,  # Exceed max_length by 1
+            'recipe_name': 'A' * 121,  # Exceed max_length
             'ingredient_name': 'Noodles',
             'chart_type': '#1'
         })
-        self.assertFalse(form.is_valid(), "Form should be invalid for recipe_name exceeding max_length.")
-
-
-
-class RecipeFormTest(TestCase):
-    def test_valid_form(self):
-        form = RecipesSearchForm(data={
-            'recipe_name': 'Pasta',
-            'ingredient_name': 'sauce',
-            'chart_type': '#1'
-        })
-        self.assertTrue(form.is_valid())
-
-    def test_invalid_form(self):
-        form = RecipesSearchForm(data={
-            'recipe_name': '',
-            'ingredient_name': '',
-            'chart_type': ''
-        })
         self.assertFalse(form.is_valid())
+
 
 class RecipeListViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = User.objects.create_user(username='testuser', password='password')
-        cls.recipe1 = Recipe.objects.create(name="Pasta", ingredients="sauce, noodles", cooking_time=20, difficulty="Easy")
-        cls.recipe2 = Recipe.objects.create(name="Pizza", ingredients="dough, cheese", cooking_time=30, difficulty="Medium")
+        Recipe.objects.create(name='Pasta', ingredients='sauce, noodles', cooking_time=15, difficulty='Easy')
+        Recipe.objects.create(name='Pizza', ingredients='cheese, dough', cooking_time=30, difficulty='Medium')
 
     def setUp(self):
         self.client.login(username='testuser', password='password')
@@ -102,12 +71,50 @@ class RecipeListViewTest(TestCase):
     def test_recipe_list_view_accessible(self):
         response = self.client.get(reverse('recipes:recipes_list'))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recipes/recipes_list.html')
 
     def test_search_functionality(self):
         response = self.client.get(reverse('recipes:recipes_list'), {'recipe_name': 'Pasta'})
-        self.assertContains(response, "Pasta", msg_prefix="Search results should include 'Pasta'.")
-        self.assertNotContains(response, "Pizza", msg_prefix="Search results should not include 'Pizza'.")
+        self.assertContains(response, 'Pasta')
+        self.assertNotContains(response, 'Pizza')
 
     def test_chart_display(self):
         response = self.client.get(reverse('recipes:recipes_list'), {'chart_type': '#1'})
         self.assertEqual(response.status_code, 200)
+        self.assertIn('chart', response.context)
+
+
+class RecipeDetailViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username='testuser', password='password')
+        cls.recipe = Recipe.objects.create(
+            name='Pasta', 
+            ingredients='sauce, noodles', 
+            cooking_time=15, 
+            difficulty='Easy'
+        )
+
+    def setUp(self):
+        self.client.login(username='testuser', password='password')
+
+    def test_recipe_detail_view(self):
+        response = self.client.get(reverse('recipes:recipes_detail', args=[self.recipe.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recipes/recipes_detail.html')
+        self.assertContains(response, self.recipe.name)
+        self.assertContains(response, self.recipe.ingredients)
+        self.assertContains(response, self.recipe.cooking_time)
+        self.assertContains(response, self.recipe.difficulty)
+
+    def test_invalid_recipe_detail_view(self):
+        response = self.client.get(reverse('recipes:recipes_detail', args=[999]))
+        self.assertEqual(response.status_code, 404)
+
+
+class HomePageTest(TestCase):
+    def test_home_page_accessible(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recipes/recipes_home.html')
+
