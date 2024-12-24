3b6a5ed45aa9e1a02001af97af09ba44ad73af7b
shareifjones <shareifjones@gmail.com>
2024-12-24 09:41:45 -0500

    Fix module paths and settings for Heroku

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes.apps.RecipesConfig',
-    # 'recipes',
+    'recipes',
 ]
 
 MIDDLEWARE = [
